699e940b4ff6f2b6c0e9b1b47cc98d8cc589f4af
Bugra <Bugra@DESKTOP-4GUJBMQ>
2022-12-27 05:12:09 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace vtarac
+{
+    partial class AracRezerve
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlistele = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnguncelle = new System.Windows.Forms.Button();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtbaslangic = new System.Windows.Forms.DateTimePicker();
+            this.dtbitis = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textid = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textrezervasyon = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnarama = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnlistele
+            // 
+            this.btnlistele.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnlistele.Location = new System.Drawing.Point(527, 40);
+            this.btnlistele.Name = "btnlistele";
+            this.btnlistele.Size = new System.Drawing.Size(157, 36);
+            this.btnlistele.TabIndex = 0;
+            this.btnlistele.Text = "Listele";
+            this.btnlistele.UseVisualStyleBackColor = true;
+            this.btnlistele.Click += new System.EventHandler(this.btnlistele_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(447, 255);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnsil
+            // 
+            this.btnsil.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsil.Location = new System.Drawing.Point(527, 283);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(157, 36);
+            this.btnsil.TabIndex = 2;
+            this.btnsil.Text = "Sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // btnguncelle
+            // 
+            this.btnguncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnguncelle.Location = new System.Drawing.Point(527, 241);
+            this.btnguncelle.Name = "btnguncelle";
+            this.btnguncelle.Size = new System.Drawing.Size(157, 36);
+            this.btnguncelle.TabIndex = 3;
+            this.btnguncelle.Text = "Güncelle";
+            this.btnguncelle.UseVisualStyleBackColor = true;
+            this.btnguncelle.Click += new System.EventHandler(this.btnguncelle_Click);
+            // 
+            // btnekle
+            // 
+            this.btnekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnekle.Location = new System.Drawing.Point(527, 167);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(157, 36);
+            this.btnekle.TabIndex = 4;
+            this.btnekle.Text = "Ekle";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(527, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bitiş Tarihi";
+            // 
+            // dtbaslangic
+            // 
+            this.dtbaslangic.Location = new System.Drawing.Point(600, 115);
+            this.dtbaslangic.Name = "dtbaslangic";
+            this.dtbaslangic.Size = new System.Drawing.Size(103, 20);
+            this.dtbaslangic.TabIndex = 7;
+            // 
+            // dtbitis
+            // 
+            this.dtbitis.Location = new System.Drawing.Point(600, 141);
+            this.dtbitis.Name = "dtbitis";
+            this.dtbitis.Size = new System.Drawing.Size(103, 20);
+            this.dtbitis.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(499, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Kullanıcı ID";
+            // 
+            // textid
+            // 
+            this.textid.Location = new System.Drawing.Point(577, 89);
+            this.textid.Name = "textid";
+            this.textid.Size = new System.Drawing.Size(72, 20);
+            this.textid.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(499, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Başlangıç Tarihi";
+            // 
+            // textrezervasyon
+            // 
+            this.textrezervasyon.Location = new System.Drawing.Point(600, 215);
+            this.textrezervasyon.Name = "textrezervasyon";
+            this.textrezervasyon.Size = new System.Drawing.Size(103, 20);
+            this.textrezervasyon.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(494, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Rezervasyon No";
+            // 
+            // btnarama
+            // 
+            this.btnarama.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnarama.Location = new System.Drawing.Point(655, 89);
+            this.btnarama.Name = "btnarama";
+            this.btnarama.Size = new System.Drawing.Size(48, 20);
+            this.btnarama.TabIndex = 17;
+            this.btnarama.Text = "Ara";
+            this.btnarama.UseVisualStyleBackColor = true;
+            this.btnarama.Click += new System.EventHandler(this.btnarama_Click);
+            // 
+            // AracRezerve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 335);
+            this.Controls.Add(this.btnarama);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textrezervasyon);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtbitis);
+            this.Controls.Add(this.dtbaslangic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.btnguncelle);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnlistele);
+            this.Name = "AracRezerve";
+            this.Text = "Arac Rezervasyon";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlistele;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnguncelle;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtbaslangic;
+        private System.Windows.Forms.DateTimePicker dtbitis;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textid;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textrezervasyon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnara;
+        private System.Windows.Forms.Button btnarama;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace vtarac
+{
+    public partial class AracRezerve : Form
+    {
+        public AracRezerve()
+        {
+            InitializeComponent();
+
+        }
+        NpgsqlConnection baglanti = new NpgsqlConnection("server = localHost; port= 5432; Database=AracKiralamaUygulamasi; user ID= postgres; password = bugra*03");
+        
+        void liste()
+        { 
+        string sorgu = "select * from rezervasyon";
+        NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btnlistele_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from rezervasyon";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu,baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("insert into rezervasyon (kullanicino,baslangictarihi,bitistarihi) values (@p1,@p2,@p3)", baglanti);
+            komut1.Parameters.AddWithValue("@p1", int.Parse(textid.Text));
+            komut1.Parameters.AddWithValue("@p2", dtbaslangic.Value.Date);
+            komut1.Parameters.AddWithValue("@p3", dtbitis.Value.Date);
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+            liste();
+            MessageBox.Show("Rezervasyonunuz Başarılı Bir Şekilde Oluşturuldu","Bilgi", MessageBoxButtons.OK);
+        }
+
+        private void btnguncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("update rezervasyon set baslangictarihi=@p1,bitistarihi=@p2 where rezervasyonno=@p3", baglanti);
+            komut3.Parameters.AddWithValue("@p1", dtbaslangic.Value.Date);
+            komut3.Parameters.AddWithValue("@p2", dtbitis.Value.Date);
+            komut3.Parameters.AddWithValue("@p3", int.Parse(textrezervasyon.Text));
+            komut3.ExecuteNonQuery();
+            baglanti.Close();
+            liste();
+            MessageBox.Show("Randevunuz Güncellendi","Bilgi", MessageBoxButtons.OK);
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("Delete from rezervasyon where rezervasyonno = @p1", baglanti);
+            komut2.Parameters.AddWithValue("@p1", int.Parse(textrezervasyon.Text));
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+            liste();
+            MessageBox.Show("Rezervasyonunuz Silindi","Bilgi",MessageBoxButtons.OK);
+        }
+
+        private void btnarama_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            int musteriid = int.Parse(textid.Text);
+            string sql = "select * from rezervasyon where kullanicino = '" + musteriid + "'";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            baglanti.Close();
+        }
+    }
+}
